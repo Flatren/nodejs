41abda5917041607f1a5ea7e8dd16b91919f76d9
Flatren <45997535+Flatren@users.noreply.github.com>
2023-05-04 14:31:15 +0500

    w

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: true},
+    host: true,
+    port: process.env.PORT},
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
